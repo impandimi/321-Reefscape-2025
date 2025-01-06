a915fb70b635f31468c480654a71e89f364d0b4d
BananasAmIRite <jasony1230@gmail.com>
2025-01-06 16:21:11 -0500

    added a logger for talonfx motors

--- src/main/java/frc/robot/util/SparkMaxLogger.java
+++ src/main/java/frc/robot/util/SparkMaxLogger.java
@@ -3,8 +3,6 @@ package frc.robot.util;
 import com.revrobotics.spark.SparkMax;
 
 import edu.wpi.first.epilogue.CustomLoggerFor;
-import edu.wpi.first.epilogue.Epilogue;
-import edu.wpi.first.epilogue.Logged;
 import edu.wpi.first.epilogue.logging.ClassSpecificLogger;
 import edu.wpi.first.epilogue.logging.EpilogueBackend;
 
@@ -17,16 +15,14 @@ public class SparkMaxLogger extends ClassSpecificLogger<SparkMax> {
 
     @Override
     protected void update(EpilogueBackend dataLogger, SparkMax object) {
-        if (Epilogue.shouldLog(Logged.Importance.DEBUG)) {
-            dataLogger.log("CAN ID", object.getDeviceId());
-            dataLogger.log("Requested Speed (Duty Cycle)", object.get());
-            dataLogger.log("Motor Voltage (V)", object.getAppliedOutput());
-            dataLogger.log("Output Current (A)", object.getOutputCurrent());
-            dataLogger.log("Motor Temperature (C)", object.getMotorTemperature());
-            dataLogger.log("Bus Voltage (V)", object.getBusVoltage());
-            dataLogger.log("Encoder Position", object.getEncoder().getPosition());
-            dataLogger.log("Encoder Velocity", object.getEncoder().getVelocity());
-        }
+        dataLogger.log("CAN ID", object.getDeviceId());
+        dataLogger.log("Requested Speed (Duty Cycle)", object.get());
+        dataLogger.log("Supply Voltage (V)", object.getBusVoltage());
+        dataLogger.log("Motor Voltage (V)", object.getAppliedOutput() * object.getBusVoltage());
+        dataLogger.log("Output Current (A)", object.getOutputCurrent());
+        dataLogger.log("Motor Temperature (C)", object.getMotorTemperature());
+        dataLogger.log("Encoder Position", object.getEncoder().getPosition());
+        dataLogger.log("Encoder Velocity", object.getEncoder().getVelocity());
     }
     
 }
