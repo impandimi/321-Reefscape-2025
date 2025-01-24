d0aab5fa02541e2932111588fb93283ab58db7ba
auberginegine <8280187@philasd.org>
2025-01-24 17:35:24 -0500

    fix(coral end effector): deleted unused import

--- src/main/java/frc/robot/subsystems/coralendeffector/CoralEndEffectorIOSim.java
+++ src/main/java/frc/robot/subsystems/coralendeffector/CoralEndEffectorIOSim.java
@@ -1,16 +1,15 @@
 /* (C) Robolancers 2025 */
 package frc.robot.subsystems.coralendeffector;
+
 import edu.wpi.first.epilogue.Logged;
 import edu.wpi.first.units.measure.Voltage;
 
-import static edu.wpi.first.units.Units.Volts;
-
-@Logged 
+@Logged
 class CoralEndEffectorIOSim implements CoralEndEffectorIO {
 
   @Override
   public void updateInputs(CoralEndEffectorInputs inputs) {}
 
   @Override
-  public void setVoltage(Voltage volts) {}
+  public void setVoltage(Voltage voltage) {}
 }
