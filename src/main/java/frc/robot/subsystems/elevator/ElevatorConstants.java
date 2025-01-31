c03dd3ee7c743010fe080552d4ae42fd8d84341f
RemiIqbal <remi.iqbal@gmail.com>
2025-01-31 15:52:50 -0500

    feat(motors): Added support for Talon&Spark motors (can be swapped out with ease (I think???).

--- src/main/java/frc/robot/subsystems/elevator/ElevatorConstants.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorConstants.java
@@ -5,6 +5,7 @@ import static edu.wpi.first.units.Units.Inches;
 import static edu.wpi.first.units.Units.Meters;
 import static edu.wpi.first.units.Units.Pound;
 
+import com.ctre.phoenix6.signals.InvertedValue;
 import edu.wpi.first.units.measure.Distance;
 import edu.wpi.first.units.measure.Mass;
 
@@ -13,7 +14,7 @@ public class ElevatorConstants {
   // List of Elevator specific constants that
   public static final int kLeftMotorID = 12;
   public static final int kRightMotorID = 0;
-  public static final boolean kInverted = false;
+  public static final InvertedValue kInverted = InvertedValue.Clockwise_Positive;
   public static final int kCurrentLimit = 40;
   public static final double kElevatorGearing = 20;
   public static final Distance kElevatorConversion = Inches.of(0.375 * 2 * 13);
@@ -22,6 +23,7 @@ public class ElevatorConstants {
   public static final double kVelocityConversionFactor = kPositionConversionFactor / 60;
   public static final Mass kElevatorCarriageMass = Pound.of(20);
   public static final Distance kElevatorDrumRadius = kElevatorConversion.div(2 * Math.PI);
+  // TODO:I bet if we measure we can get a more accurate minimum height :)
   public static final Distance kElevatorMinimumHeight = Inches.of(27);
   public static final Distance kElevatorMaximumHeight = Inches.of(56);
   public static final Distance kElevatorStartingHeight = kElevatorMinimumHeight;
---
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorIOTalon.java
@@ -0,0 +1,76 @@
+/* (C) Robolancers 2025 */
+package frc.robot.subsystems.elevator;
+
+import static edu.wpi.first.units.Units.Amps;
+import static edu.wpi.first.units.Units.Meters;
+import static edu.wpi.first.units.Units.MetersPerSecond;
+import static edu.wpi.first.units.Units.Volt;
+import static edu.wpi.first.units.Units.Volts;
+
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.hardware.TalonFX;
+import edu.wpi.first.epilogue.Logged;
+import edu.wpi.first.units.measure.Distance;
+import edu.wpi.first.units.measure.Voltage;
+
+@Logged
+// For when Elevator is real
+public class ElevatorIOTalon implements ElevatorIO {
+  // Creates config record w/ values
+  public static final ElevatorConfig config = new ElevatorConfig(150, 0, 0.3, 0.43, 0.64);
+
+  // Creates motor objects
+
+  public TalonFX elevatorMotorLeft = new TalonFX(ElevatorConstants.kLeftMotorID);
+
+  // NOTE: Right motor MAY be commented out in order to test one motor
+
+  // public TalonFX elevatorMotorRight = new TalonFX(ElevatorConstants.kRightMotorID);
+
+  // Constructor: Sets up motors
+  public ElevatorIOTalon() {
+    setupMotors();
+  }
+
+  // Updates inputs with values from encoder (Called periodically in periodic function later)
+  public void updateInputs(ElevatorInputs inputs) {
+    inputs.height =
+        Meters.of(
+            elevatorMotorLeft.getPosition().getValueAsDouble()
+                * ElevatorConstants.kPositionConversionFactor);
+    inputs.velocity =
+        MetersPerSecond.of(
+            elevatorMotorLeft.getVelocity().getValueAsDouble()
+                * ElevatorConstants.kVelocityConversionFactor);
+    inputs.current = Amps.of(elevatorMotorLeft.getStatorCurrent().getValueAsDouble());
+  }
+
+  // Method to setup L & R motor & encoders
+  // NOTE: Right motor follows left & only left motor encoder is used
+  private void setupMotors() {
+    TalonFXConfiguration configuration = new TalonFXConfiguration();
+    configuration.CurrentLimits.SupplyCurrentLimit = ElevatorConstants.kCurrentLimit;
+    configuration.MotorOutput.Inverted = ElevatorConstants.kInverted;
+    configuration.Feedback.SensorToMechanismRatio = ElevatorConstants.kPositionConversionFactor;
+    elevatorMotorLeft.getConfigurator().apply(configuration);
+
+    // elevatorMotorRight.getConfigurator().apply(configuration);
+    // elevatorMotorRight.setControl(new Follower(elevatorMotorLeft.getDeviceID(), false));
+
+  }
+
+  // Sets power of motors w/voltage
+  public void setVoltage(Voltage Volts) {
+    elevatorMotorLeft.setVoltage(Volts.in(Volt));
+  }
+
+  // Sets encoder pos
+  public void setEncoderPosition(Distance position) {
+    elevatorMotorLeft.setPosition(position.in(Meters));
+  }
+
+  // Special case where encoder pos is reset to the initial/starting height
+  public void resetEncoderPosition() {
+    setEncoderPosition(ElevatorConstants.kElevatorStartingHeight);
+  }
+}
