dd641b5910b4d7d2376a605fe6a06665e068bf75
at <atxponh@gmail.com>
2025-02-08 10:35:20 -0500

    fix(assets): added assets, removed driver controls, commeted out assets calibration poses

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,24 +1,16 @@
 /* (C) Robolancers 2025 */
 package frc.robot;
 
-import static edu.wpi.first.units.Units.Degrees;
-import static edu.wpi.first.units.Units.MetersPerSecond;
-import static edu.wpi.first.units.Units.RadiansPerSecond;
-
 import edu.wpi.first.epilogue.Logged;
-import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.subsystems.SuperstructureVisualizer;
 import frc.robot.subsystems.algaeIntakePivot.AlgaeIntakePivot;
-import frc.robot.subsystems.algaeIntakePivot.AlgaeIntakePivotConstants;
 import frc.robot.subsystems.algaeIntakeRollers.AlgaeIntakeRollers;
 import frc.robot.subsystems.coralendeffector.CoralEndEffector;
-import frc.robot.subsystems.drivetrain.DrivetrainConstants;
 import frc.robot.subsystems.drivetrain.SwerveDrive;
 import frc.robot.subsystems.elevator.Elevator;
-import frc.robot.subsystems.elevator.ElevatorConstants;
 import frc.robot.subsystems.elevatorarm.ElevatorArm;
 
 @Logged
@@ -39,36 +31,6 @@ public class RobotContainer {
           () -> elevator.getHeight(), () -> elevatorArm.getAngle(), () -> algaePivot.getAngle());
 
   public RobotContainer() {
-
-    drivetrain.setDefaultCommand(
-        drivetrain.driveFieldCentric(
-            () ->
-                -MathUtil.applyDeadband(
-                    driver.getLeftY() * DrivetrainConstants.kMaxLinearVelocity.in(MetersPerSecond),
-                    DrivetrainConstants.kDriveDeadband),
-            () ->
-                -MathUtil.applyDeadband(
-                    driver.getLeftX() * DrivetrainConstants.kMaxLinearVelocity.in(MetersPerSecond),
-                    DrivetrainConstants.kDriveDeadband),
-            () ->
-                -MathUtil.applyDeadband(
-                    driver.getRightX()
-                        * DrivetrainConstants.kMaxAngularVelocity.in(RadiansPerSecond),
-                    DrivetrainConstants.kRotationDeadband)));
-
-    driver.y().onTrue(elevator.goToHeight(() -> ElevatorConstants.kElevatorMinimumHeight)); // v
-    driver.x().onTrue(elevator.goToHeight(() -> ElevatorConstants.kElevatorMaximumHeight)); // c
-
-    driver.b().onTrue(elevatorArm.goToAngle(() -> Degrees.of(90))); // x
-    driver.a().onTrue(elevatorArm.goToAngle(() -> Degrees.of(0))); // z
-
-    driver
-        .rightBumper()
-        .onTrue(algaePivot.goToAngle(() -> AlgaeIntakePivotConstants.kPivotFloorAngle));
-    driver
-        .leftBumper()
-        .onTrue(algaePivot.goToAngle(() -> AlgaeIntakePivotConstants.kPivotIntakeAngle));
-
     configureBindings();
   }
 
